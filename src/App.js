80b1a2e43d5d062bd8cc17420ce3e1db2159eb09
Sara <38770404+SaraDrwish@users.noreply.github.com>
2023-09-19 15:03:24 +0300

    prtbk f_fullStk_App_react_nodjs_1 css 3 and routs

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import Header from './componants/header/Header';
 import Login from './componants/Login/Login';
 import Logout from './componants/logout/Logout.jsx';
 import Register from './componants/register/Register.jsx';
+import Blogs from "./pages/Blogs/Blogs.jsx"
+import BlogDetails from "./pages/BlogDetailes/BlogDetails.jsx"
+import UserBlog from "./pages/UserBlog/UserBlog.jsx"
+import CreateBlog from "./pages/CreateBlog/CreateBlog.jsx"
+import { Route, Routes } from 'react-router-dom';
  
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
-         <Login />
+        <Routes>
+          <Route path="/" element={<Blogs />} />
+          <Route path="/myBlogs" element={<UserBlog />} />
+          <Route path="/CreateBlog" element={<CreateBlog />} />
+          <Route path="/blogDetails/:id" element={< BlogDetails />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+         {/* <Login /> */}
         {/* <Logout /> */}
         {/* <Register/> */}
         <Footer/>
--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,13 @@
 import axios from "axios";
 
+const Api = axios.create({
+    baseURL : "http://localhost:5000/api",
+    headers: {
+        "Content-Type" : "application/json"
+    },
+    withCredentials : true
 
+})
+
+export default Api
 
